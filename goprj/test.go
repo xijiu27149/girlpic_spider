74642e4ae0f304c61c23104afc83dd17711fefac
xijiu27149 <xijiu27149@hotmail.com>
2022-08-03 12:20:04 +0800

    删除文件

--- goprj/test.go
+++ goprj/test.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	cur_class, _ := strconv.Atoi(os.Args[1])
-	cur_page, _ := strconv.Atoi(os.Args[2])
-	cur_item, _ := strconv.Atoi(os.Args[3])
-	cur_subpage, _ := strconv.Atoi(os.Args[4])
-	fmt.Printf("%d_%d_%d_%d", cur_class, cur_page, cur_item, cur_subpage)
+	filepath := "C:\\Users\\dujw\\Downloads\\file.txt"
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		str, _ := reader.ReadString('\n')
+
+		err = os.Remove(str)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("成功__" + str)
+		}
+	}
 }
